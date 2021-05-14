61f8cbe6112eca30662388add59f08f005952641
AleOlea <espiritusdelfuego@gmail.com>
2021-05-14 16:08:34 -0400

    FIX: Index + Cli

--- cli.js
+++ cli.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 //EVERYTHING WILL BE IMPORTED HERE
 const mdLinks = require("./index.js");
-const getUserInfo = require("./sayName")
-
-getUserInfo.sayName();
-getUserInfo.sayAddress();
-
-
 
 mdLinks(process.argv[2])
   
-//HELLO
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 let fs = require("fs")
 let path = require ("path")
-//Read a file and print content to the console
 
+//Read a file and print content to the console
 const mdLinks = (path) => {
-  //validar si es archivo o carpeta
-// saber si es ruta absoluta o relativa libreria o terminal
+
   fs.readFile( path, "utf8", (err, data) => {
     if(err) {
       console.log("error: ", err)
