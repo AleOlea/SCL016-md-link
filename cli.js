88a265dacb4ef36b5366a3a168f5e9cb03e1b81e
AleOlea <espiritusdelfuego@gmail.com>
2021-05-16 11:22:40 -0400

    Style

--- cli.js
+++ cli.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
-//EVERYTHING WILL BE IMPORTED HERE
-const isValidPath = require("is-valid-path");
 const mdLinks = require("./index.js");
-const isValidInput = require("./is-valid");
-// const isAbs = require("./is-absolute")
+const validateInput = require("./is-abs")
 
+const validInput = validateInput()
 
-
-//verify file path does not contain any invalid characters or is empty
-const isValid = isValidPath();
-
-
-
-// Read the file and print content to the console
+// // Read the file and print content to the console
 const input = mdLinks(process.argv[2])
   
