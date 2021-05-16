8a95283c1543b40d4018397a2623352449cae793
AleOlea <espiritusdelfuego@gmail.com>
2021-05-16 11:25:09 -0400

    Feat: is valid is in

--- is-abs.js
+++ is-abs.js
@@ -2,8 +2,14 @@ let fs = require("fs")
 let path = require ("path")
 let files = process.argv[2]
 let resolve = require('path').resolve
+const isValid = require('is-valid-path');
 
 const validateInput = () => {
+
+//Returns true if a file path does not contain any invalid characters or is empty
+const isValIn = isValid(files)
+console.log("nada raro")
+
 //file exists
 fs.access(files, fs.F_OK, (err) => {
   if (err) {
@@ -14,13 +20,18 @@ fs.access(files, fs.F_OK, (err) => {
 })
 //path is absolute?
 const isAbs = path.isAbsolute(files)
-console.log("path is abs:", isAbs)
+console.log("path is absolute?:", isAbs)
 if (path.isAbsolute(files) === false){
 //make it absolute
 const makeItAbs= resolve(files)
-console.log("make-it-abs", makeItAbs)
+console.log("MAKE IT ABS:", makeItAbs)
  } else {
-   console.log("is allright")
+   console.log("already absolute")
  }
+ return isAbs
 }
-validateInput()
+// validateInput()
+
+module.exports = validateInput;
+
+
