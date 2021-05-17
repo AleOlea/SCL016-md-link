3559473902073a62864f2498b3d9854a60f60a5d
AleOlea <espiritusdelfuego@gmail.com>
2021-05-17 11:42:15 -0400

    Docs: impleneting flow

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// esto si funciona, la cambie para incorporar el flow if md
 let fs = require("fs")
 let path = require ("path")
+let files = process.argv[2]
 
-// Read the file and print content to the console
-const mdLinks = (path) => {
+const mdLinks = () => {
+
+const ext = path.extname(files)
+console.log("The extension is gui gui gui:", ext)
+  if(ext === ".md"){
+    console.log("wow")
 
 new Promise
   ((resolve, reject) => {
-    fs.readFile(path, "utf8", (err, data) => {
+    fs.readFile(files, "utf8", (err, data) => {
       if(err) {
         reject(err)
     } else {
@@ -22,6 +28,40 @@ new Promise
 .catch(err => {
   console.log(err)
 })
+} else {
+  console.log("no md files here!")
 }
-
+}
+// mdLinks()
 module.exports = mdLinks;
+
+
+
+//Preguntar a Sergio
+//esta es la version que me recomendó la Ely tiene el path en cli pero no me funcionaba con el if md
+// let fs = require("fs")
+// let path = require ("path")
+
+// // Read the file and print content to the console
+// const mdLinks = (path) => {
+
+
+// new Promise
+//   ((resolve, reject) => {
+//     fs.readFile(path, "utf8", (err, data) => {
+//       if(err) {
+//         reject(err)
+//     } else {
+//       console.log("Bueno, aqui esta el contenido de tu carpeta:")
+//       resolve(data)
+//       return data  
+//       }
+//     })
+//   })
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(err => {
+//   console.log(err)
+// })
+// }
