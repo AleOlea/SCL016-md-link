a74928617d62c8ee8ddcd2bea40966c4fb30687d
AleOlea <espiritusdelfuego@gmail.com>
2021-05-27 19:07:08 -0400

    Feat: cleaning code 2

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const mdLinks = require('./index2');
 //   .catch((error) => console.log(error));
 
 /*
-CLI
-node app --validate --stats
+CLI --validate --stats
 */
 
-mdLinks(process.argv[2], process.argv.slice(3)) // porque silce 3 y no 3, 5
+mdLinks(process.argv[2], process.argv.slice(3))
   .then((res) => console.log(res))
   .catch((error) => console.log(error));
