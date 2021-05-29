7f3364f3749c5d86ddba4c3098a42409095410d1
AleOlea <espiritusdelfuego@gmail.com>
2021-05-28 20:03:49 -0400

    Style

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const mdLinks = require('./index2');
+const mdLinks = require('./index');
 
 /*
- API
- */
+ API  case false / case true
+*/
 
-// mdLinks(path, { validate: false }) // false or true
-//   .then((res) => console.log(res))
-//   .catch((error) => console.log(error));
+mdLinks(process.argv[2], { validate: false })
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
 
 /*
 CLI --validate --stats
 */
 
-mdLinks(process.argv[2], process.argv.slice(3))
-  .then((res) => console.log(res))
-  .catch((error) => console.log(error));
+// mdLinks(process.argv[2], process.argv.slice(3))
+//   .then((res) => console.log(res))
+//   .catch((error) => console.log(error));
