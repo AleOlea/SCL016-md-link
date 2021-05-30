5e5c21716ed1e6e780d1662d9dd655f13cdd8784
AleOlea <espiritusdelfuego@gmail.com>
2021-05-30 14:30:43 -0400

    Deleted

--- test/valida-url.spec.js
+++
@@ -1,7 +0,0 @@
-// const validaUrl = require('../index2.js');
-
-// describe('validaUrl', () => {
-//   it('should be a function', () => {
-//     expect(typeof validaUrl(). toBe("function")) 
-//   });
-// });
