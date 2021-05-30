82c23a70892f0c297b333b8f3f2d4c1b366ee7c7
AleOlea <espiritusdelfuego@gmail.com>
2021-05-30 14:31:42 -0400

    Docs: md-test

---
+++ test/md-links.spec.js
@@ -0,0 +1,9 @@
+// const mdLinks = require('../index2.js');
+
+// // jest.mock('node-fetch');
+// // const fetch = require('node-fetch');
+// describe('mdLinks, reads path and gives options', () => {
+//   return mdLinks().then(data =>
+//     expect(data). toBe("va"))
+//   });
+// });
